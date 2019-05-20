2b8d3ada13955f5902c1a065f802a8a3c3d54c4c
tripleCC <triplec.linux@gmail.com>
2019-05-20 23:41:34 +0800

    update link-checker

--- link-checker.rb
+++ link-checker.rb
@@ -10,19 +10,19 @@ doc = Nokogiri::HTML(html)
 links = doc.search('a').map { |link| link }
 invalid_links = []
 Parallel.each(links, in_threads: 10) do |link|
-	url = link.attributes['href'].value
-	begin
-		RestClient.head(url)
-	rescue RestClient::ResourceNotFound
-		invalid_links << link
-	end
+  url = link.attributes['href'].value
+  begin
+    RestClient.head(url)
+  rescue RestClient::ResourceNotFound
+    invalid_links << link
+  end
 end
 
 if invalid_links.any?
-	puts '无效链接：'
-	puts '============================'
-	invalid_links.each { |link| puts link.content }
-	puts '============================'
+  puts '无效链接：'
+  puts '============================'
+  invalid_links.each { |link| puts link.content }
+  puts '============================'
 
-	raise '需移除如上无效链接'
+  raise '需移除如上无效链接'
 end
