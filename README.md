e78ef6e8e2c14b2afec211ff2af1c9fc4d180cea
tripleCC <triplec.linux@gmail.com>
2019-05-20 23:29:01 +0800

    add CI

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 [Kernel Programming Guide](https://developer.apple.com/library/archive/documentation/Darwin/Conceptual/KernelProgramming/keepout/keepout.html#//apple_ref/doc/uid/TP30000905-CH205-TPXREF108)
 
 [Threading Programming Guide](https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/Multithreading/Introduction/Introduction.html#//apple_ref/doc/uid/10000057i-CH1-SW1)
-
---
+++ link-checker.rb
@@ -0,0 +1,28 @@
+require 'rest_client'
+require 'redcarpet'
+require 'parallel'
+require 'nokogiri'
+
+readme = File.open('./README.md')
+markdown = Markdown.new(readme.read)
+html = markdown.to_html
+doc = Nokogiri::HTML(html)
+links = doc.search('a').map { |link| link }
+invalid_links = []
+Parallel.each(links, in_threads: 10) do |link|
+	url = link.attributes['href'].value
+	begin
+		RestClient.head(url)
+	rescue RestClient::ResourceNotFound
+		invalid_links << link
+	end
+end
+
+if invalid_links.any?
+	puts '无效链接：'
+	puts '============================'
+	invalid_links.each { |link| puts link.content }
+	puts '============================'
+
+	raise '需移除如上无效链接'
+end
